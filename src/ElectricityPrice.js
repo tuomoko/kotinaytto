c141a3a9dcb3fe39f6130cfb756e11479fd2c5dd
Tuomo Kohtamaki <tuomo.kohtamaki@gmail.com>
2022-09-25 13:07:05 +0300

    Updated config example

--- src/ElectricityPrice.js
+++ src/ElectricityPrice.js
@@ -1,25 +1,26 @@
 // Fetch data from own API that uses ENTSO-E Transparency API
 import React from "react";
+import { useSettings } from './SettingsContext';
 
 function ElectricityPrice()
 {
-  const api_url = "https://vider.kapsi.fi/entsoprice/"
+  const {electricityConfig} = useSettings();
   const [data,setData]=React.useState([]);
-
-  const getData=()=>{
-    fetch(api_url)
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
+  
   React.useEffect(()=>{
+    const getData=()=>{
+      fetch(electricityConfig.API_URL)
+        .then(function(response){
+          console.log(response)
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(myJson);
+          setData(myJson)
+        });
+    }
     getData()
-  },[])
+  },[electricityConfig])
   
   return data;
 }
